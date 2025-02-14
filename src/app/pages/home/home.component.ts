81f7dde423dc6c7d84f898a48edf4241950f9418
victor <vgomez@emfasi.com>
2025-02-14 17:37:52 +0100

    my experiencia

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,13 @@ import { HomeSliderComponent } from './components/home-slider/home-slider.compon
 import { skip } from 'rxjs';
 import { SkillsComponent } from './components/skills/skills.component';
 import { AboutComponent } from "../about/about.component";
+import { ExperienceComponent } from './components/experience/experience.component';
+import { SeparatorComponent } from './components/separator/separator.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HomeSliderComponent, SkillsComponent, AboutComponent],
+  imports: [HomeSliderComponent, SkillsComponent, ExperienceComponent, SeparatorComponent, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
