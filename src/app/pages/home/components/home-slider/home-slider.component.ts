f34ad003aa12b156e0ca3e6ea0a308c0e941759e
victor <vgomez@emfasi.com>
2025-02-14 14:00:47 +0100

    header y primer block home

---
+++ src/app/pages/home/components/home-slider/home-slider.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-slider',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-slider.component.html',
+  styleUrl: './home-slider.component.scss'
+})
+export class HomeSliderComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { HomeSliderComponent } from './components/home-slider/home-slider.component';
+import { skip } from 'rxjs';
+import { SkillsComponent } from './components/skills/skills.component';
+import { AboutComponent } from "../about/about.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HomeSliderComponent, SkillsComponent, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
