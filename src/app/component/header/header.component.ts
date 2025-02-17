0efa6b88a32c39db02207099d04d397b6586b319
victor <vgomez@emfasi.com>
2025-02-17 09:22:49 +0100

    menú

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -9,5 +9,36 @@ import { RouterLink } from '@angular/router';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isOpen = false;
 
+  toggleMenu() {
+    this.isOpen = !this.isOpen;
+    this.updateMenuState();
+  }
+
+  closeMenu() {
+    this.isOpen = false;
+    this.updateMenuState();
+  }
+
+  private updateMenuState() {
+    const svg = document.querySelector('.header_hamburguer');
+    const list = document.querySelector('.menu');
+
+    if (svg) {
+      svg.classList.toggle('menu-open', this.isOpen);
+    }
+
+    if (list) {
+      list.classList.toggle('menu-open', this.isOpen);
+    }
+  }
+  ngAfterViewInit() {
+    const menuLinks = document.querySelectorAll('.menu a');
+    menuLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        this.closeMenu();
+      });
+    });
+  }
 }
